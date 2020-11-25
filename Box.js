d172f59eff3d8f08094395988a85a6d22a4e198d
mrinalini06092006 <73584416+mrinalini06092006@users.noreply.github.com>
2020-11-25 22:26:33 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box{
+    constructor(x,y,width,height){
+        
+    this.body=Bodies.rectangle(x,y,width,height);
+    World.add(world,this.body);
+    } 
+    
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("red");
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+        
+    
+    }
+    
+    }
+   
